50885162689b293558dee6740733c14ac1f14901
tommasobertagnin <tommasobertagnin@gmail.com>
2017-06-07 08:10:57 +0200

    use single db connection

--- server/index.js
+++ server/index.js
@@ -9,9 +9,19 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', require('./routes'))
-app.use('/api/users', require('./api/users'))
-app.use('/api/polls', require('./api/polls'))
+const DB_URL = process.env.NODE_ENV === 'production'
+  ? process.env.DB_URL_PRODUCTION
+  : process.env.DB_URL_LOCAL
+
+mongoose.connect(DB_URL)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', () => {
+  app.use('/', require('./routes'))
+  app.use('/api/users', require('./api/users'))
+  app.use('/api/polls', require('./api/polls'))
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server is Live!')
