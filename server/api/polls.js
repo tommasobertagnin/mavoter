30d141804df2a0aeabf38826af1bbb29e4b3e34d
tommasobertagnin <tommasobertagnin@gmail.com>
2017-06-07 08:03:49 +0200

    remove local db connection

--- server/api/polls.js
+++ server/api/polls.js
@@ -4,59 +4,48 @@ const Poll = require('./poll.model')
 
 // TODO: add polls tests
 
-const URL = process.env.NODE_ENV === 'production'
-  ? process.env.DB_URL_PRODUCTION
-  : process.env.DB_URL_LOCAL
-
-mongoose.connect(URL)
-const db = mongoose.connection
-
-db.once('open', () => {
-  pollsRouter.get('/list', (req, res) => {
-    Poll.find({})
-      .limit(35)
-      .exec((err, polls) => {
-        // TODO: handle error
-        if (err) {
-          return res.json(err)
-        }
-        res.json(polls)
-      })
-  })
-
-  pollsRouter.get('/poll/:id', (req, res) => {
-    Poll.findOne({ _id: req.params.id })
-      .exec((err, poll) => {
-        // TODO: handle error
-        if (err) {
-          return res.json(err)
-        }
-        res.json(poll)
-      })
-  })
-
-  /* PRIVATE ENDPOINTS */
-  // TODO: PROTECT ENDPOINTS WITH AUTH MIDDLEWARE
-
-  pollsRouter.post('/new-poll', (req, res) => {
-    const {
-      title,
-      options,
-      username
-    } = req.body
-
-    const newPoll = new Poll({ title, options, username })
-    
-    newPoll.save((err, result) => {
+pollsRouter.get('/list', (req, res) => {
+  Poll.find({})
+    .limit(35)
+    .exec((err, polls) => {
       // TODO: handle error
       if (err) {
         return res.json(err)
       }
-      res.json(result)
+      res.json(polls)
     })
-  })
 })
 
-db.on('error', console.error.bind(console, 'connection error:'))
+pollsRouter.get('/poll/:id', (req, res) => {
+  Poll.findOne({ _id: req.params.id })
+    .exec((err, poll) => {
+      // TODO: handle error
+      if (err) {
+        return res.json(err)
+      }
+      res.json(poll)
+    })
+})
+
+/* PRIVATE ENDPOINTS */
+// TODO: PROTECT ENDPOINTS WITH AUTH MIDDLEWARE
+
+pollsRouter.post('/new-poll', (req, res) => {
+  const {
+    title,
+    options,
+    username
+  } = req.body
+
+  const newPoll = new Poll({ title, options, username })
+  
+  newPoll.save((err, result) => {
+    // TODO: handle error
+    if (err) {
+      return res.json(err)
+    }
+    res.json(result)
+  })
+})
 
 module.exports = pollsRouter
--- server/api/users.js
+++ server/api/users.js
@@ -1,21 +1,10 @@
 const mongoose = require('mongoose')
 const usersRouter = require('express').Router()
 
-const URL = process.env.NODE_ENV === 'production'
-  ? process.env.DB_URL_PRODUCTION
-  : process.env.DB_URL_LOCAL
-
-mongoose.connect(URL)
-const db = mongoose.connection
-
-db.once('open', () => {
-  usersRouter.post('/login', (req, res) => {
-    const { username, password } = req.body
-    // TODO: IMPLEMENT LOGIN
-    res.json({ ok: `trying to log in as: ${username}@${password}` })
-  })
+usersRouter.post('/login', (req, res) => {
+  const { username, password } = req.body
+  // TODO: IMPLEMENT LOGIN
+  res.json({ ok: `trying to log in as: ${username}@${password}` })
 })
-
-db.on('error', console.error.bind(console, 'connection error:'))
 
 module.exports = usersRouter
